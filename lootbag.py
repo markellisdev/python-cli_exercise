cf1b51f6baf6cb7a83466183a877c0a7ee546367
Mark Ellis <markellisdev@gmail.com>
2017-01-18 10:09:24 -0600

    getting all children, but name duplicates

--- lootbag.py
+++ lootbag.py
@@ -36,7 +36,7 @@ class LootBag():
 			except sqlite3.OperationalError:
 				pass
 
- # This was code to make test pass
+ # This was code to make test pass -----------------------------------------
 	# 	try:
 	# 		self.good_children[child].append(toy) #Trying to append, if not key error
 
@@ -61,31 +61,17 @@ class LootBag():
 				pass
 
 
-# This was code to make test pass
+# This was code to make test pass ------------------------------------------
 # 		try:
 # 			self.good_children[child].remove(toy)
 # 		except KeyError:
 # 			pass
 
-	def list_good_children(self):
-
-		with sqlite3.connect('lootbag.db') as conn:
-			c = conn.cursor()
-
-			c.execute("select Name from Child")
-			results = c.fetchall()
-
-
-
-
-# Original code to pass test
-# 		return [kid for kid in self.good_children.keys()] #returning list of all (keys) children
-
 	def get_toy_by_child(self, child):
 		with sqlite3.connect('lootbag.db') as conn:
 			c = conn.cursor()
 
-#Enclosing in three " enables writing multi-line string
+# --- Enclosing in three " enables writing multi-line string ---
 			c.execute("""SELECT t.Name
 				FROM Toy t, Child c
 				WHERE c.Name='{}'
@@ -96,20 +82,41 @@ class LootBag():
 
 			print(toys)
 
- # This was code to make test pass
+ # This was code to make test pass ----------------------------------
 	# 	return [child for child in self.good_children.keys()]
 
 	# def get_child(self):
 	# 	return self.toy
 
+	def list_all_children(self):
 
+		with sqlite3.connect('lootbag.db') as conn:
+			c = conn.cursor()
 
+			c.execute("""SELECT c.Name
+				FROM Child c, Toy t
+				WHERE t.ToyId
+				AND c.ChildId = t.ChildId
+			""")
+			results = c.fetchall()
+
+			print(results) #This works but repeats name. Change to set to only have one
+
+# Original code to pass test
+# 		return [kid for kid in self.good_children.keys()] #returning list of all (keys) children
 
 
 if __name__ == "__main__":
 	bag = LootBag()
+	print(sys.argv)
 	if sys.argv[1] == "add":
 		bag.add_toy_for_child(sys.argv[2], sys.argv[3])
 		print(bag.get_toy_by_child(sys.argv[3]))
 	elif sys.argv[1] == "remove":
-		bag.remove_toy_from_child(sys.argv[2, sys.argv[3]])
+		bag.remove_toy_from_child(sys.argv[2], sys.argv[3])
+		print(bag.get_toy_by_child(sys.argv[3]))
+	elif sys.argv[1] == "ls":
+		bag.get_toy_by_child(sys.argv[2])
+	elif sys.argv[1] == "ls_all":
+		bag.list_all_children()
+
